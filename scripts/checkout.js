883313d33fa3742359465cf84f572ea1878c703f
Rukasu96 <lukaszkulesza@op.pl>
2024-12-02 18:22:19 +0100

    Added header info if cart is empty

--- scripts/checkout.js
+++ scripts/checkout.js
@@ -10,43 +10,51 @@ document.addEventListener("DOMContentLoaded", () => {
 function renderCartProducts(){
   checkCartProducts();
   let cartProductsHTML = '';
-  cartProducts.forEach((cartProduct) => {
-  cartProductsHTML += 
-  `
-  <div class="ProductContainer">
-    <div class="ProductCardImageContainer">
-      <img src="/images/boots.jpg" class="ProductImage">
-    </div>
-    <div class="ProductCardPropertiesContainer">
-      <span>
-        Name: ${cartProduct.product.name}
-      </span>
-      <span class="ProductDescirption">
-        Description: ${cartProduct.product.description}.
-      </span>
-      <span>
-        Rating: ${cartProduct.product.rating}
-      </span>
-      <span>
-        Price: ${cartProduct.product.price}
-      </span>
-    </div>
-    <span class="ProductCardButtonRemoveContainer">
-      <button class="CheckoutCardButton jsRemoveItemButton" data-product-id = "${cartProduct.product.id}">Remove</button>
-    </span>
-  </div>
-  `
-  });
 
-  document.querySelector('.jsCartProductContainer').innerHTML = cartProductsHTML;
 
-  document.querySelectorAll('.jsRemoveItemButton').forEach((button) => {
-    button.addEventListener('click', () => {
-      const productId = button.dataset.productId;
-      cart.removeFromCart(productId);
-      renderCartProducts();
-    });
-  });
+  if(Object.keys(cartProducts).length === 0){
+    cartProductsHTML += "Your cart is empty";
+    document.querySelector(".jsHeaderEmptyCart").innerHTML = cartProductsHTML;
+    document.querySelector('.jsCartProductContainer').innerHTML = '';
+  }else{
+    cartProducts.forEach((cartProduct) => {
+      cartProductsHTML += 
+      `
+      <div class="ProductContainer">
+        <div class="ProductCardImageContainer">
+          <img src="/images/boots.jpg" class="ProductImage">
+        </div>
+        <div class="ProductCardPropertiesContainer">
+          <span>
+            Name: ${cartProduct.product.name}
+          </span>
+          <span class="ProductDescirption">
+            Description: ${cartProduct.product.description}.
+          </span>
+          <span>
+            Rating: ${cartProduct.product.rating}
+          </span>
+          <span>
+            Price: ${cartProduct.product.price}
+          </span>
+        </div>
+        <span class="ProductCardButtonRemoveContainer">
+          <button class="CheckoutCardButton jsRemoveItemButton" data-product-id = "${cartProduct.product.id}">Remove</button>
+        </span>
+      </div>
+      `
+      });
+    
+      document.querySelector('.jsCartProductContainer').innerHTML = cartProductsHTML;
+    
+      document.querySelectorAll('.jsRemoveItemButton').forEach((button) => {
+        button.addEventListener('click', () => {
+          const productId = button.dataset.productId;
+          cart.removeFromCart(productId);
+          renderCartProducts();
+        });
+      });
+  }
 }
 
 function checkCartProducts(){
